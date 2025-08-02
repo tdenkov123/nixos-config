732643a813d3437abcc73a0c1bab662e42daa2f7
tdenkov123 <tdenkov123@mail.ru>
2025-08-02 15:55:37 +0300

    fixed greetd

--- nixos-mods/greetd.nix
+++ nixos-mods/greetd.nix
@@ -6,7 +6,7 @@ in {
     enable = true;
     settings = {
       default_session = {
-        command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session} --asterisks --asterisks-char * --greet-align center --theme 000000 --theme-bg 282828 --theme-fg d4be98 --theme-input-fg d4be98 --theme-input-bg 3c3836 --theme-spinner d4be98";
+        command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session} --asterisks --asterisks-char * --greet-align center";
         user = user;
       };
     };
