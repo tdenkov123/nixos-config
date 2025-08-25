53937baba33bdbd76eb7df5a5ea55899d3d00451
Timofey Bardenkov <tdenkov123@mail.ru>
2025-08-25 18:34:10 +0300

    hyprpanel

--- home-manager/configs/hyprpanel/default.nix
+++ home-manager/configs/hyprpanel/default.nix
@@ -1,63 +1,36 @@
-{ pkgs, unstablePkgs, ... }: {
+{ inputs, ... }: {
   programs.hyprpanel = {
-    enable = true;
-    package = unstablePkgs.hyprpanel;
-    
-    configFile = ./hyprpanel.conf;
-    
-    styleFile = ./style.css;
-  };
+    settings = {
+      layout = {
+        bar.layouts = {
+          "0" = {
+            left = [ "dashboard" "workspaces" ];
+            middle = [ "media" ];
+            right = [ "volume" "systray" "notifications" ];
+          };
+        };
+      };
 
-  home.file.".local/bin/qs-toggle" = {
-    text = ''#!/usr/bin/env bash
-# Quick settings toggle script for hyprpanel
-if [[ "$1" == "wifi" ]]; then
-  if nmcli radio wifi | grep -qi enabled; then 
-    nmcli radio wifi off
-  else 
-    nmcli radio wifi on
-  fi
-elif [[ "$1" == "bluetooth" ]]; then
-  if bluetoothctl show | grep -q "Powered: yes"; then 
-    bluetoothctl power off
-  else 
-    bluetoothctl power on
-  fi
-elif [[ "$1" == "vpn" ]]; then
-  if nmcli -t -f TYPE connection show --active | grep -q '^vpn$'; then
-    nmcli -t -f NAME,TYPE connection show --active | awk -F: '$2=="vpn"{print $1}' | while read -r c; do
-      [ -n "$c" ] && nmcli connection down "$c"
-    done
-  else
-    first=$(nmcli -t -f NAME,TYPE connection show | awk -F: '$2=="vpn"{print $1; exit}')
-    [ -n "$first" ] && nmcli connection up "$first"
-  fi
-elif [[ "$1" == "airplane" ]]; then
-  if rfkill | awk '{print $4}' | grep -q yes; then 
-    rfkill unblock all
-  else 
-    rfkill block all
-  fi
-fi
-'';
-    executable = true;
-  };
+      bar.launcher.autoDetectIcon = true;
+      bar.workspaces.show_icons = true;
 
-  home.file.".local/bin/brightness-set" = {
-    text = ''#!/usr/bin/env bash
-set -e
-val="$1"
-[ -n "$val" ] && brightnessctl set "$val%"
-'';
-    executable = true;
-  };
+      menus.clock = {
+        time = {
+          military = true;
+          hideSeconds = true;
+        };
+        weather.unit = "metric";
+      };
 
-  home.file.".local/bin/volume-set" = {
-    text = ''#!/usr/bin/env bash
-set -e
-val="$1"
-[ -n "$val" ] && wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ "$val%"
-'';
-    executable = true;
+      menus.dashboard.directories.enabled = false;
+      menus.dashboard.stats.enable_gpu = true;
+
+      theme.bar.transparent = true;
+
+      theme.font = {
+        name = "CaskaydiaCJetBrains Mono";
+        size = "16px";
+      };
+    };
   };
 }
