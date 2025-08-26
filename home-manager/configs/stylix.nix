f732d1c19ca98fa959af26992da5cf87f50b4368
Timofey Bardenkov <tdenkov123@mail.ru>
2025-08-26 20:06:27 +0300

    wallpapers

--- home-manager/configs/stylix.nix
+++ home-manager/configs/stylix.nix
@@ -73,6 +73,6 @@
     #   url = "https://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/raw/branch/master/png/gruvbox-dark-rainbow.png";
     #   sha256 = "036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
     # };
-    image = ./../wallpapers/bunta.jpg;
+    image = ./../wallpapers/initiald_2.jpg;
   };
 }
