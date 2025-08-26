f87034235b995bf197de815d0fcfc22f9375641d
Timofey Bardenkov <tdenkov123@mail.ru>
2025-08-26 20:04:38 +0300

    wallpapers

--- home-manager/configs/stylix.nix
+++ home-manager/configs/stylix.nix
@@ -69,9 +69,10 @@
       light = "Papirus-Light";
     };
 
-    image = pkgs.fetchurl {
-      url = "https://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/raw/branch/master/png/gruvbox-dark-rainbow.png";
-      sha256 = "036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
-    };
+    # image = pkgs.fetchurl {
+    #   url = "https://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/raw/branch/master/png/gruvbox-dark-rainbow.png";
+    #   sha256 = "036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
+    # };
+    image = ./../wallpapers/bunta.jpg;
   };
 }
